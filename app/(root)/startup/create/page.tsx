0db8be3e8b3c0f12dc6a0f230911e0dead1988da
WASSIMKAROUI <wassimkaroui541@gmail.com>
2025-04-30 16:04:42 +0100

    First Commit

---
+++ app/(root)/startup/create/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import StartupForm from '@/components/StartupForm'
+import { redirect } from 'next/navigation';
+import { auth } from '@/auth';
+
+const page = async () => {
+  const session = await auth();
+
+  if(!session) {
+    redirect('/');
+  } 
+  return (
+    <>
+    <section className='pink_container !min-h-[230px]'>
+      <h1 className='heading'>
+        SUBMIT YOUR STARTUP
+      </h1>
+      </section>
+      <StartupForm />
+    </>
+  )
+}
+
+export default page
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7ff0f6b3a689b18cdd2b408343411001@o4508550923550720.ingest.de.sentry.io/4508550932004944",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
